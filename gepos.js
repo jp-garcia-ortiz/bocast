6a97a150a9cec1c6be8e130fff99cb311d674838
jp-garcia-ortiz <jp.garcia.ortiz@gmail.com>
2014-12-14 23:55:11 +0100

    Updating

--- gepos.js
+++ gepos.js
@@ -2,18 +2,15 @@ var url = require('url');
 var http = require('http');
 var cmd = require('commander');
 
-
-function decodeUrl(urlStr)
-{
+function decodeUrl(urlStr) {
   var urlObj = url.parse(urlStr);
 
   return {
-	host: urlObj.hostname,
-	port: (urlObj.port ? urlObj.port : '80'),
-    path: urlObj.path
-  }; 	
+    host : urlObj.hostname,
+    port : (urlObj.port ? urlObj.port : '80'),
+    path : urlObj.path
+  };
 }
-
 
 cmd
   .version('1.0')
@@ -25,22 +22,22 @@ cmd
 var postReq = null;
 var ctype = 'content-type';
 
-if(cmd.post) {
+if (cmd.post) {
   var postOpts = decodeUrl(cmd.post);
   postOpts.method = 'POST';
 
   postReq = http.request(postOpts, function(res) {
-	res.setEncoding('binary');
-	  
-    res.on('data', function (chunk) {
+    res.setEncoding('binary');
+
+    res.on('data', function(chunk) {
       console.log('Response: ' + chunk);
     });
   });
-  
+
   postReq.setHeader(ctype, 'application/octet-stream');
 }
 
-if(cmd.get) {
+if (cmd.get) {
   var getOpts = decodeUrl(cmd.get);
   getOpts.method = 'GET';
 
@@ -49,36 +46,36 @@ if(cmd.get) {
 
   http.request(getOpts, function(res) {
     console.log('\n', res.headers, '\n');
-  
+
     header = res.headers[ctype];
-  
+
     res.setEncoding('binary');
-  
-    res.on('data', function (chunk) {
+
+    res.on('data', function(chunk) {
       console.log('[', typeof chunk, '] Received', chunk.length, 'bytes');
-    
-      if(postReq) {
-        if(header) {
+
+      if (postReq) {
+        if (header) {
           postReq.setHeader(ctype, header);
           header = null;
         }
-    	
+
         postReq.write(chunk, 'binary');
       }
-    
+
       total += chunk.length;
-    
-      if(cmd.limit) {
-        if(total >= cmd.limit)
-          res.destroy();  
+
+      if (cmd.limit) {
+        if (total >= cmd.limit)
+          res.destroy();
       }
     });
 
-    res.on('end', function () {
-	  console.log('\nTotal received =', total, 'bytes\n');
-	
-	  if(postReq) 
-	    postReq.end();
-    });	
+    res.on('end', function() {
+      console.log('\nTotal received =', total, 'bytes\n');
+
+      if (postReq)
+        postReq.end();
+    });
   }).end();
 }
